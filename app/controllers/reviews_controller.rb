f13ce3566f77228c9ce11b4945e6df1734897a4e
Tafara Mafemba <tafara.maf@gmail.com>
2024-12-30 15:54:10 -0800

    add review rating logic

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,12 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user = User.find(params[:user_id]) # Associate review with seller
+    @
 
     if @review.save
-      redirect_back fallback_location: product_path(params[:product_id]), notice: "Review added successfully."
+      redirect_to products_path
     else
-      redirect_back fallback_location: product_path(params[:product_id]), alert: @review.errors.full_messages.to_sentence
+      redirect_to products_path alert: @review.errors.full_messages.to_sentence
     end
   end
 
