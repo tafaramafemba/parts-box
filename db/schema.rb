66312e7e0c92ca0252898594ee2dd29ca05afec9
Tafara Mafemba <tafara.maf@gmail.com>
2024-12-29 18:15:11 -0800

    style sign in and up forms and confirmable to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_21_222809) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_29_234209) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -321,6 +321,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_21_222809) do
     t.string "paypal_refresh_token"
     t.string "first_name"
     t.string "last_name"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
