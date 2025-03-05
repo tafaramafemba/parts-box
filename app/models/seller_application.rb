14445f5fc7e859bc06b9c9c9b3c9104a5ac83b30
Tafara Mafemba <tafara.maf@gmail.com>
2025-03-05 13:50:59 -0800

    add seller application validations

--- app/models/seller_application.rb
+++ app/models/seller_application.rb
@@ -2,4 +2,9 @@ class SellerApplication < ApplicationRecord
   belongs_to :user
   has_one_attached :id_document
   has_one_attached :business_registration_document
+
+  validates :email, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true 
+  validates :address, presence: true
+  validates :business_registration_number, uniqueness: true
 end
